37833b1bb25bc993169b2633a3e7b7890247c792
hawka11 <keith.hawkridge@gmail.com>
2014-10-08 17:59:03 +1100

    eh

--- fsm-accessor-hazelcast/src/main/java/simple/fsm/accessor/hazelcast/HazelcastStateMachineAccessor.java
+++ fsm-accessor-hazelcast/src/main/java/simple/fsm/accessor/hazelcast/HazelcastStateMachineAccessor.java
@@ -1,5 +1,10 @@
 package simple.fsm.accessor.hazelcast;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import simple.fsm.core.Context;
 import simple.fsm.core.StateMachine;
 import simple.fsm.core.accessor.StateMachineAccessor;
@@ -10,6 +15,8 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class HazelcastStateMachineAccessor implements StateMachineAccessor {
+
+    private final Logger LOG = LoggerFactory.getLogger(HazelcastStateMachineAccessor.class);
 
     @Override
     public String create(State initialState, Context context) {
@@ -23,7 +30,19 @@ public class HazelcastStateMachineAccessor implements StateMachineAccessor {
 
     @Override
     public Optional<Lock> tryLock(String stateMachineId, long timeout, TimeUnit timeUnit) {
+        java.util.concurrent.locks.Lock lock = getHazel().getLock("STATE_MACHINE-" + stateMachineId);
+        lock.lock();
+        try {
+
+        } finally {
+            lock.unlock();
+        }
         return null;
+    }
+
+    private HazelcastInstance getHazel() {
+        Config config = new Config();
+        return Hazelcast.newHazelcastInstance(config);
     }
 
     @Override
