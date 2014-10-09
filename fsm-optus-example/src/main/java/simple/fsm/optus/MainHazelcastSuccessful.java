a25c2175b7cbee6839e91884e6105bfb60a26f2b
hawka11 <keith.hawkridge@gmail.com>
2014-10-09 18:51:00 +1100

    eh.....inject  client hz

--- fsm-optus-example/src/main/java/simple/fsm/optus/MainHazelcastSuccessful.java
+++ fsm-optus-example/src/main/java/simple/fsm/optus/MainHazelcastSuccessful.java
@@ -1,31 +1,36 @@
 package simple.fsm.optus;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.config.Config;
+import com.hazelcast.config.SerializerConfig;
 import com.hazelcast.core.Hazelcast;
-import simple.fsm.accessor.hazelcast.HazelcastStateMachineAccessor;
+import com.hazelcast.core.HazelcastInstance;
 import simple.fsm.core.StateMachine;
-import simple.fsm.core.accessor.InMemoryStateMachineAccessor;
 import simple.fsm.core.accessor.StateMachineAccessor;
 import simple.fsm.core.template.BaseStateMachineCallback;
 import simple.fsm.core.template.StateMachineTemplate;
+import simple.fsm.hazelcast.Serializer.BaseJacksonSerializer;
+import simple.fsm.hazelcast.accessor.HazelcastStateMachineAccessor;
 import simple.fsm.optus.event.RechargeAcceptedEvent;
 import simple.fsm.optus.event.RequestRechargeEvent;
 import simple.fsm.optus.state.InitialState;
 
 import java.math.BigDecimal;
+import java.util.Collection;
 
 public class MainHazelcastSuccessful {
 
     public static void main(String[] args) throws Exception {
-        //Distributed HZ cluster running somewhere
+        //Distributed HZ server cluster running somewhere
         new Thread(() -> {
             Config config = new Config();
-             Hazelcast.newHazelcastInstance(config);
+            Hazelcast.newHazelcastInstance(config);
         }).run();
 
         Thread.sleep(2000);
 
-        final StateMachineAccessor accessor = new HazelcastStateMachineAccessor();
+        final StateMachineAccessor accessor = new HazelcastStateMachineAccessor(createClientHz());
         final StateMachineTemplate template = new StateMachineTemplate(accessor);
 
         //create state machine
@@ -60,4 +65,25 @@ public class MainHazelcastSuccessful {
         //This 'current' state could be inspected by anything, which could react as desired / or send their own event to sm etc...
         System.out.println("Ending.... current state is: " + accessor.get(stateMachineId).getCurrentState().getClass().getSimpleName());
     }
+
+    private static HazelcastInstance createClientHz() {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.addAddress("127.0.0.1:5701");
+
+       // Collection<SerializerConfig> serializerConfig = clientConfig.getSerializationConfig().getSerializerConfigs();
+        //serializerConfig.add(serializerConfig(1, StateMachine.class));
+        //serializerConfig.add(new SerializerConfig().setTypeClass(StateMachine.class).setImplementation(new SerializationFactory.StateMachineSerializer()));
+        //serializerConfig.add(new SerializerConfig().setTypeClass(InitialState.class).setImplementation(new SerializationFactory.InitialStateSerializer()));
+
+        return HazelcastClient.newHazelcastClient(clientConfig);
+    }
+
+    /*private static SerializerConfig serializerConfig(int typeid, Class<?> klass) {
+        return new SerializerConfig().setTypeClass(klass).setImplementation(new BaseJacksonSerializer(klass) {
+            @Override
+            public int getTypeId() {
+                return typeid;
+            }
+        });
+    }*/
 }
