5d4cc16b08829cb5929df766e8abd4a086476029
Keith Hawkridge <keith.hawkridge@touchcorp.com>
2014-10-28 20:24:09 +1100

    use context directly in template, don't worry about extra getters.

--- fsm-telco-example-user-input/src/main/java/pure/fsm/telco/user/application/api/StateMachineViewFactory.java
+++ fsm-telco-example-user-input/src/main/java/pure/fsm/telco/user/application/api/StateMachineViewFactory.java
@@ -6,13 +6,11 @@ import pure.fsm.core.state.ErrorFinalState;
 import pure.fsm.core.state.State;
 import pure.fsm.core.state.SuccessFinalState;
 import pure.fsm.core.state.TimedOutFinalState;
-import pure.fsm.telco.user.domain.TelcoRechargeContext;
 import pure.fsm.telco.user.domain.state.InitialState;
 import pure.fsm.telco.user.domain.state.WaitingForConfirmationState;
 
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 import static com.google.common.collect.Maps.newHashMap;
 
@@ -55,20 +53,12 @@ public class StateMachineViewFactory {
             super(templateName);
         }
 
-        void setContext(Context sm) {
-            this.context = sm;
+        void setContext(Context context) {
+            this.context = context;
         }
 
         public Context getContext() {
             return context;
-        }
-
-        public Set<String> getConfirmedPins() {
-            return ((TelcoRechargeContext) getContext()).getConfirmedPins();
-        }
-
-        public Set<String> getWaitingPins() {
-            return ((TelcoRechargeContext) getContext()).nonConfirmedPins();
         }
     }
 
